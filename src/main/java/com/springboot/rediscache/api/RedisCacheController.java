9fc396a3f32d253e5991f99545c8fa44fc14fd8a
Cem <cemkok@outlook.com>
2022-11-20 17:55:55 +0300

    redis cache clear was added

--- src/main/java/com/springboot/rediscache/api/RedisCacheController.java
+++ src/main/java/com/springboot/rediscache/api/RedisCacheController.java
@@ -11,11 +11,21 @@ import com.springboot.rediscache.service.RedisCacheService;
 @RequestMapping("/test")
 public class RedisCacheController {
 	
+	private int sayac = 0;
+	
 	@Autowired
 	private RedisCacheService redisCacheService;
 	
 	@GetMapping
 	public String cacheControl() throws InterruptedException {
+		if(sayac == 5) {
+			
+			redisCacheService.clearCache();
+			
+			sayac=0;
+			
+		}
+		sayac++;
 		return redisCacheService.longRunningMethod();	
 		
 		
--- src/main/java/com/springboot/rediscache/service/RedisCacheService.java
+++ src/main/java/com/springboot/rediscache/service/RedisCacheService.java
@@ -1,15 +1,23 @@
 package com.springboot.rediscache.service;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisCacheService {
 	
+	
+	int sayac = 0;
 	@Cacheable(cacheNames = "mySpecialCache")
 	public String longRunningMethod() throws InterruptedException {
 		Thread.sleep(5000L);
 		return "method çalıştı";	
+	}
+	
+	@CacheEvict(cacheNames= "mySpecialCache" )
+	public void clearCache() {
+		System.out.println("cache temizledi");
 	}
 
 }
